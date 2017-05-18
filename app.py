fcecd683416f9ccd8096a75444248f69d0311dff
Jonathan Barrios <jeb2239@columbia.edu>
2017-05-18 17:22:13 -0400

    stuff

--- app.py
+++ app.py
@@ -6,12 +6,15 @@ from flask_heroku import Heroku
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/pre-registration'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     return 'Yo, its working!'
+
+
 
 
 
