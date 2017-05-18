5a1c2eee877d9c6da2c2d9001e914564e5bb4f89
Jonathan Barrios <jeb2239@columbia.edu>
2017-05-18 17:01:58 -0400

    pg

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
 
+from flask import Flask, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
+
+from flask.ext.heroku import Heroku
+
+app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/pre-registration'
+heroku = Heroku(app)
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     return 'Yo, its working!'
+
 
 
 if __name__ == "__main__":
