306970bd921b307ea6bfbd0ed76ee160d0483531
Jonathan Barrios <jeb2239@columbia.edu>
2017-05-18 17:08:59 -0400

    reqs

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 
-from flask.ext.heroku import Heroku
+from flask_heroku import Heroku
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/pre-registration'
